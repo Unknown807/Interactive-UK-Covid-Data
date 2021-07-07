0d1ed58ea8397f934fc241e8c70cf2ddba4030a4
Unknown807 <milvovangveric@gmail.com>
2021-07-07 12:10:15 +0100

    Testing map plotting and reading data with ggplot2

--- ui.R
+++ ui.R
@@ -1,2 +1,11 @@
 
-ui <- navbarPage(title="Title")
+ui <- navbarPage(title="UK Covid Data Visualised",
+  tabPanel(title="Maps"
+  ),
+  tabPanel(title="Other Option 1"
+  ),
+  tabPanel(title="Other Option 2",
+  ),
+  tabPanel(title="About",
+  )
+)
